8a124e539dd0e0f78a543c898a4bf8f54c419bf4
Marcin <gasiorek.mail@gmail.com>
2023-02-09 12:52:41 +0100

    Creating Interface Mappable and methods within it

--- src/dev/marcin/Main.java
+++ src/dev/marcin/Main.java
@@ -1,3 +1,5 @@
+package dev.marcin;
+
 public class Main {
     public static void main(String[] args) {
 
---
+++ src/dev/marcin/Mappable.java
@@ -0,0 +1,18 @@
+package dev.marcin;
+
+public interface Mappable {
+    String JSON_PROPERTY = """
+            "properties": {%s} """;
+    String getLabel();
+    Geometry getShape();
+    String getMarker();
+    default String toJSON(){
+        return """
+                "type": "%s", "label": "%s", "marker": "%s" """
+                .formatted(getShape(), getLabel(), getMarker());
+
+    }
+    static void mapIt(Mappable mappable){
+        System.out.println(JSON_PROPERTY.formatted(mappable.toJSON()));
+    }
+}
